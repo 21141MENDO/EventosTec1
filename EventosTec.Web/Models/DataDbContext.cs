c5cf947f279ccfcef923adafc77301f4f8441007
21141MENDO <39782111+21141MENDO@users.noreply.github.com>
2020-05-20 18:01:12 -0700

    Create DataBase and First Model

---
+++ EventosTec.Web/Models/DataDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EventosTec.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventosTec.Web.Models
+{
+    public class DataDbContext:DbContext
+    {// se crea un constructor con el mismo nombre de la clase,  es para la conexion con la base de datos
+        public DataDbContext(DbContextOptions<DataDbContext> options):base(options)
+        {
+
+        }
+        //get servira para hacer conexion y optener la conexion
+        public DbSet<Client> clients { get; set; }
+    }
+}
